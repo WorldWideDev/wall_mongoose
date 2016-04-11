a7b8b23b83a2daed00039f752ad7ba538474a1d8
Devon Newsom <dev206@gmail.com>
2016-04-11 12:41:04 -0700

    some basic styling edits

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res){
-	Message.find({}).populate('comments').exec(function (err, messages){
+	Message.find({}).sort('-createdAt').populate('comments').exec(function (err, messages){
 		if(err){
 			res.json(err)
 		}else{
